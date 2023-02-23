2ff0ace3fdb3d82bfb336ce6aa56bc0f40431d20
Hannah Smith <hls68@byu.edu>
2023-02-23 12:57:44 -0700

    Add project files.

---
+++ Authentication_Lab414/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Authentication_Lab414.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Authentication_Lab414.Areas.Identity.IdentityHostingStartup))]
+namespace Authentication_Lab414.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
